e6527596a1dd195fa75635f9349a9d2ddb068074
Jobin Philip <jobinphilip68@gmail.com>
2023-08-02 23:02:45 +0530

    Added necessary changes to display TodoItems on the index page

--- src/main/java/dev/jobinphilip/todoapp/controller/TodoItemController.java
+++ src/main/java/dev/jobinphilip/todoapp/controller/TodoItemController.java
@@ -2,14 +2,24 @@ package dev.jobinphilip.todoapp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import dev.jobinphilip.todoapp.repository.TodoItemRepository;
 
 @Controller
 public class TodoItemController {
 
     private static final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
+
+    private final TodoItemRepository todoItemRepository;
+
+    @Autowired
+    public TodoItemController(TodoItemRepository todoItemRepository) {
+        this.todoItemRepository = todoItemRepository;
+    }
 
     @GetMapping("/")
     public ModelAndView showIndexPage(){
@@ -19,6 +29,7 @@ public class TodoItemController {
         logger.info("Received GET request for /index");
 
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
 
         logger.debug("Rendering the index page.");
         return modelAndView;
--- src/main/java/dev/jobinphilip/todoapp/model/TodoItem.java
+++ src/main/java/dev/jobinphilip/todoapp/model/TodoItem.java
@@ -8,11 +8,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class TodoItem {
     
     @Id
